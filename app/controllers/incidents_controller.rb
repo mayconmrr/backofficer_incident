dc7ff670aedaeb65a5117e6f976d91898b8dcddc
mayconmrr <maycon.mrr@gmail.com>
2018-04-16 09:55:27 -0300

    added idea in git ignore

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,10 +1,11 @@
 class IncidentsController < ApplicationController
-  before_action :set_incident, only: [:update, :edit, :destroy, :show, :analyse, :capture, :reopen, :solve, :pending]
+  before_action :set_incident,only: [:update, :edit, :destroy, :show, :analyse, :capture, :reopen, :solve, :pending]
   before_action :authenticate_user, only: [:my_incidents, :search]
   before_action :authenticate_backofficer!, only: [:create]
   before_action :authenticate_analyst!, only: [:analyse, :solve, :capture]
 
   before_action :check_to_reopen, only: [:reopen]
+  before_action :check_to_solve, only: [:solve]
   include UserHelper
 
   def show; end
@@ -61,15 +62,25 @@ class IncidentsController < ApplicationController
 
   def analyse
     @incident.update(status: Enumerations::IncidentStatus::ANALYSING, analyst: current_analyst, analysis_started_at: DateTime.now)
+    flash[:notice] = 'Requisição em análise'
     redirect_to @incident
   end
 
   def solve
+    binding.pry
+    if @incident.update(incident_params)
+      format.html { redirect_to @incident, notice: 'Requisição resolvida com sucesso!' }
+      format.json { render :show, status: :ok }
+    else
+      format.html { render :new }
+      format.json { render json: @incident.errors, status: :unprocessable_entity }
+    end
+
     redirect_to @incident unless check_to_solve
   end
 
   def pending
-    #
+    flash[:notice] = 'Requisição atualizada para pendente.'
   end
 
   def capture
@@ -78,7 +89,8 @@ class IncidentsController < ApplicationController
   end
 
   def reopen
-    # colocar reopen flag to true
+
+    flash[:notice] = 'Requisição reaberta!'
   end
 
   def search
@@ -106,9 +118,9 @@ class IncidentsController < ApplicationController
   end
 
   def authenticate_user
-    if check_user.class.name == "Backofficer"
+    if check_user.class.name == 'Backofficer'
       :authenticate_backofficer!
-    elsif check_user.class.name == "Analyst"
+    elsif check_user.class.name == 'Analyst'
       :authenticate_analyst!
     else
       redirect_to new_backofficer_session_path
@@ -116,7 +128,12 @@ class IncidentsController < ApplicationController
   end
 
   def check_to_solve
-    @incident.status == Enumerations::IncidentStatus::ANALYSING && @incident.analyst == current_analyst
+    if @incident.status == Enumerations::IncidentStatus::ANALYSING && @incident.analyst == current_analyst
+      true
+    else
+      flash[:danger] = 'Você não tem permissão para resolver essa requisição.'
+      redirect_to @incident
+    end
   end
 
   def check_to_reopen
